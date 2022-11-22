80bf6be473fc25de4f654ca75396414db7ebbe31
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-11-22 15:09:15 +0200

    edit the responsiveness for last item of projects list

--- dataPortfolioItems.js
+++ dataPortfolioItems.js
@@ -1,5 +1,23 @@
 let dataPortfolioItems = [
     {
+        title: "Link In Bio",
+        imageLink: "images/linkin-bio.PNG",
+        liveLink: "https://bogdan-muntean.github.io/LinkInBio/",
+        repoLink: "https://github.com/bogdan-muntean/LinkInBio",
+    },
+    {
+        title: "BMI Calculator",
+        imageLink: "images/bmi.PNG",
+        liveLink: "https://bogdan-muntean.github.io/BMI-Calculator-Project/",
+        repoLink: "https://github.com/bogdan-muntean/BMI-Calculator-Project",
+    },
+    {
+        title: "Rock-Paper-Scissors Game",
+        imageLink: "images/r-p-s-game.PNG",
+        liveLink: "https://bogdan-muntean.github.io/Rock-Paper-Scissors_Game/",
+        repoLink: "https://github.com/bogdan-muntean/Rock-Paper-Scissors_Game",
+    },
+    {
         title: "Hangman Game",
         imageLink: "images/hangman-game.PNG",
         liveLink: "https://bogdan-muntean.github.io/Project_Hangman/",
@@ -7,11 +25,15 @@ let dataPortfolioItems = [
     },
 ]
 
+let portfolioListHtml = document.querySelector(".portfolio-list");
+
 function addPortfolioItems(container, arrayList){
     //create item's components
     let portfolioItemHtml = document.createElement('div')
+
     let imageAnchor = document.createElement('a')
     let imageHtml = document.createElement('img')
+
     let portfolioTextContainer = document.createElement('div')
     let portfolioLinks = document.createElement('div')
 
@@ -24,14 +46,16 @@ function addPortfolioItems(container, arrayList){
     imageHtml.setAttribute('src', arrayList.imageLink)
     //text container
     portfolioTextContainer.classList.add('portfolio-text')
+    portfolioLinks.classList.add('portfolio-links')
+    
+    //add content
     portfolioTextContainer.innerHTML = `
     <h4>
         <a href="${arrayList.liveLink}">
             ${arrayList.title}
         </a>
     </h4>`;
-    portfolioLinks.classList.add('portfolio-links')
-    portfolioLinks.innerHtml = `
+    portfolioLinks.innerHTML = `
     <div>
         <a href="${arrayList.repoLink}" target="_blank">
             Source 
@@ -45,14 +69,51 @@ function addPortfolioItems(container, arrayList){
             <i class="fab fa-github"></i>
         </a>
     </div>
-    `
-
+    `;
 
     //append childs to parents containers
-    portfolioTextContainer.appendChild(portfolioLinks)
     imageAnchor.appendChild(imageHtml)
+    portfolioTextContainer.appendChild(portfolioLinks)
+
+    portfolioItemHtml.appendChild(imageAnchor)
+    portfolioItemHtml.appendChild(portfolioTextContainer)
     container.appendChild(portfolioItemHtml)
 }
 
 
-let portfolioListHtml = document.querySelector(".portfolio-list");
+dataPortfolioItems.forEach( (element) => {
+    console.log("portfolio item created")
+    addPortfolioItems(portfolioListHtml, element)
+})
+
+
+// let portfolioLinkValue = `
+//     <div>
+//         <a href="${arrayList.repoLink}" target="_blank">
+//             Source 
+//             <i class="fa-solid fa-display"></i>
+//         </a>
+//     </div>
+//     <div>
+//         <a href="${arrayList.liveLink}" target="_blank">
+//             Live 
+//             <i class="fab fa-github"></i>
+//         </a>
+//     </div>
+//     `;
+//     portfolioLinks.innerHtml = portfolioLinkValue;
+    // portfolioLinks.innerHtml = `
+    // <div>
+    //     <a href="${arrayList.repoLink}" target="_blank">
+    //         Source 
+    //         <i class="fa-solid fa-display"></i>
+    //     </a>
+    // </div>
+
+    // <div>
+    //     <a href="${arrayList.liveLink}" target="_blank">
+    //         Live 
+    //         <i class="fab fa-github"></i>
+    //     </a>
+    // </div>
+    // `;
