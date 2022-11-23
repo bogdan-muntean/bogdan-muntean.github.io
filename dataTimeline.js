3b67fde78aa426788a5480edf45a6f94394eff1f
Bogdan-M-source <bogdan.muntean98@gmail.com>
2022-11-23 16:37:13 +0200

    dataTimeline modifications plus html and css of about container

--- dataTimeline.js
+++ dataTimeline.js
@@ -16,6 +16,10 @@ function addItemsToTimeline(container, dataArray){
 
     //add content
     iconItem.innerHTML = dataArray.icon;
+    if(dataArray.icon === `<i class="fa-solid fa-graduation-cap"></i>`){
+        iconItem.style.backgroundColor = "#c6c614";
+    }
+
     timeData.textContent = dataArray.timeData;
     titleHtml.innerHTML = `
     ${dataArray.title} <br>
@@ -33,9 +37,16 @@ function addItemsToTimeline(container, dataArray){
 
 let academicDataList = [
     {
-        title: `Responsive Web Design Certification`,
-        span: "freeCodeCamp",
-        timeData: "2021",
+        title: `JavaScript Algorithms and Data Structures Masterclass `,
+        span: "Udemy (Colt Steele)",
+        timeData: "11/2022 - Current",
+        text: "",
+        icon: `<i class="fa-solid fa-display"></i>`,
+    },
+    {
+        title: `JavaScript Algorithms and Data Structures Challenges`,
+        span: "Edabit",
+        timeData: "09/2022 - Current",
         text: "",
         icon: `<i class="fa-solid fa-display"></i>`,
     },
@@ -44,6 +55,27 @@ let academicDataList = [
         span: "Informal School of IT, Cluj-Napoca",
         timeData: "01/2022 – 05/2022",
         text: "I learned from the front end development course advanced notions of HTML, CSS, JavaScript and about frameworks like React and Tailwind. As well as about Git, Github, SPA and Responsive Websites.",
+        icon: `<i class="fa-solid fa-graduation-cap"></i>`,
+    },
+    {
+        title: `Responsive Web Design Certification`,
+        span: "freeCodeCamp",
+        timeData: "11/2021",
+        text: "",
+        icon: `<i class="fa-solid fa-display"></i>`,
+    },
+    {
+        title: `Front-end Foundations`,
+        span: "The Odin Project",
+        timeData: "07/2021",
+        text: "I learned HTML, CSS, JavaScript, Command Line Basics, GitHub, GIT.",
+        icon: `<i class="fa-solid fa-display"></i>`,
+    },
+    {
+        title: `Introduction to Computer Science`,
+        span: "HarvardX CS50 (2020)",
+        timeData: "06/2021",
+        text: "I learned C, HTML and CSS, the basics of machine learning, algorithms and structured data.",
         icon: `<i class="fa-solid fa-display"></i>`,
     },
     {
@@ -51,28 +83,28 @@ let academicDataList = [
         span: `"Babeș-Bolyai" University, Cluj-Napoca`,
         timeData: "2020-2021",
         text: "",
-        icon: `<i class="fa-solid fa-display"></i>`,
+        icon: `<i class="fa-solid fa-graduation-cap"></i>`,
     },
     {
         title: `Faculty of Medicine - Balneo-Physio-Kinetotherapy and Medical Recovery Specialist`,
         span: `"Iuliu Haţieganu" University of Medicine and Pharmacy, Cluj-Napoca`,
         timeData: "2017-2020",
         text: "",
-        icon: `<i class="fa-solid fa-display"></i>`,
+        icon: `<i class="fa-solid fa-graduation-cap"></i>`,
     },
     {
         title: `CERTIFIED IN COMPUTER SCIENCE`,
         span: `"Lucian Blaga" High School, Cluj-Napoca`,
         timeData: "2017",
         text: "",
-        icon: `<i class="fa-solid fa-display"></i>`,
+        icon: `<i class="fa-solid fa-graduation-cap"></i>`,
     },
     {
         title: `High School - Section of Mathematics-Informatics Intensive Informatics`,
         span: `"Lucian Blaga" High School, Cluj-Napoca`,
         timeData: "2013-2017",
         text: "Experienced with C++, C, developed algorithms and problem-solving skills. Certificate of digital skills and professional skills in computer science.",
-        icon: `<i class="fa-solid fa-display"></i>`,
+        icon: `<i class="fa-solid fa-graduation-cap"></i>`,
     },
 ];
 
