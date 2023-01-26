f86d0c1aa08b6d704ed048aedcabf7e9ebeaa611
Bogdan-M-source <bogdan.muntean98@gmail.com>
2023-01-26 13:44:40 +0200

    feat:  edits for mobile first, font and content responsive; folder for posibility-to-use; reset for all scss to rewrite clean code; more variables and less media queries; innerHtml for all portfolioItems childs; read more and read less button + functionality for project's description

--- dataPortfolioItems.js
+++ dataPortfolioItems.js
@@ -1,135 +1,136 @@
 let dataPortfolioItems = [
-    {
-        title: "Link In Bio",
-        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor illo doloribus.",
-        imageLink: "images/linkin-bio.PNG",
-        liveLink: "https://bogdan-muntean.github.io/LinkInBio/",
-        repoLink: "https://github.com/bogdan-muntean/LinkInBio",
-    },
-    {
-        title: "BMI Calculator",
-        description: "Some description",
-        imageLink: "images/bmi.PNG",
-        liveLink: "https://bogdan-muntean.github.io/BMI-Calculator-Project/",
-        repoLink: "https://github.com/bogdan-muntean/BMI-Calculator-Project",
-    },
-    {
-        title: "Rock-Paper-Scissors Game",
-        description: "Some description",
-        imageLink: "images/r-p-s-game.PNG",
-        liveLink: "https://bogdan-muntean.github.io/Rock-Paper-Scissors_Game/",
-        repoLink: "https://github.com/bogdan-muntean/Rock-Paper-Scissors_Game",
-    },
-    {
-        title: "Hangman Game",
-        description: "Some description",
-        imageLink: "images/hangman-game.PNG",
-        liveLink: "https://bogdan-muntean.github.io/Project_Hangman/",
-        repoLink: "https://github.com/bogdan-muntean/Project_Hangman",
-    },
-]
-
-
+  {
+    title: "Link In Bio",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor illo doloribus.",
+    imageLink: "images/linkin-bio.PNG",
+    liveLink: "https://bogdan-muntean.github.io/LinkInBio/",
+    repoLink: "https://github.com/bogdan-muntean/LinkInBio",
+  },
+  {
+    title: "BMI Calculator",
+    description: "Some description",
+    imageLink: "images/bmi.PNG",
+    liveLink: "https://bogdan-muntean.github.io/BMI-Calculator-Project/",
+    repoLink: "https://github.com/bogdan-muntean/BMI-Calculator-Project",
+  },
+  {
+    title: "Rock-Paper-Scissors Game",
+    description: "Some description",
+    imageLink: "images/r-p-s-game.PNG",
+    liveLink: "https://bogdan-muntean.github.io/Rock-Paper-Scissors_Game/",
+    repoLink: "https://github.com/bogdan-muntean/Rock-Paper-Scissors_Game",
+  },
+  {
+    title: "Hangman Game",
+    description: "Some description",
+    imageLink: "images/hangman-game.PNG",
+    liveLink: "https://bogdan-muntean.github.io/Project_Hangman/",
+    repoLink: "https://github.com/bogdan-muntean/Project_Hangman",
+  },
+];
 
 let portfolioListHtml = document.querySelector(".portfolio-list");
 
-function addPortfolioItems(container, arrayList){
-    //  -----   create item's components
-    let portfolioItemHtml = document.createElement('div')
+function addPortfolioItems(container, arrayList) {
+  let portfolioItemHtml = document.createElement("div");
 
-    let imageContainer = document.createElement('div')
-    let imageAnchor = document.createElement('a')
-    let imageHtml = document.createElement('img')
+  portfolioItemHtml.innerHTML = `
+    <div class="portfolio-item">
+        <div class="portfolio-image">
+            <a href=${arrayList.liveLink} target="_blank">
+                <img src=${arrayList.imageLink} alt="">
+            </a>
+        </div>
+        <div class="portfolio-text-container">
+            <div class="portfolio-title">
+                <h4>
+                    <a href="${arrayList.liveLink}">
+                        ${arrayList.title}
+                    </a>
+                </h4>
+            </div>
+            <div class="portfolio-description">
+                <p>
+                    ${arrayList.description} 
+                    <button class="readmore-btn">Read more</button>
+                    <button class="readless-btn">Read less</button>
+                </p>
+            </div>
+            <div class="portfolio-links">
+                <div>
+                    <a href="${arrayList.repoLink}" target="_blank">
+                        Source 
+                        <i class="fa-solid fa-display"></i>
+                    </a>
+                </div>
 
-    let portfolioTitleContainer = document.createElement('div')
-    let portfolioDescriptionContainer = document.createElement('div')
-    let portfolioLinks = document.createElement('div')
-    let portfolioTextContainer = document.createElement('div')
-
-    //  -----   add classes's components
-    //principal item
-    portfolioItemHtml.classList.add('portfolio-item')
-    //image
-    imageContainer.classList.add('portfolio-image')
-    imageAnchor.setAttribute('href', arrayList.liveLink)
-    imageHtml.setAttribute('src', arrayList.imageLink)
-    //text container
-    portfolioTitleContainer.classList.add('portfolio-title')
-    portfolioDescriptionContainer.classList.add('portfolio-description')
-    portfolioLinks.classList.add('portfolio-links')
-    portfolioTextContainer.classList.add('portfolio-text-container')
-    
-    //  -----   add content
-    portfolioTitleContainer.innerHTML = `
-    <h4>
-        <a href="${arrayList.liveLink}">
-            ${arrayList.title}
-        </a>
-    </h4>`;
-    portfolioDescriptionContainer.innerHTML = `<p>${arrayList.description}</p>`;
-    portfolioLinks.innerHTML = `
-    <div>
-        <a href="${arrayList.repoLink}" target="_blank">
-            Source 
-            <i class="fa-solid fa-display"></i>
-        </a>
+                <div>
+                    <a href="${arrayList.liveLink}" target="_blank">
+                        Live 
+                        <i class="fab fa-github"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
+`;
 
-    <div>
-        <a href="${arrayList.liveLink}" target="_blank">
-            Live 
-            <i class="fab fa-github"></i>
-        </a>
-    </div>
-    `;
-
-    //   ----   append childs to parents containers
-    imageContainer.appendChild(imageAnchor)
-    imageAnchor.appendChild(imageHtml)
-
-    portfolioItemHtml.appendChild(imageContainer)
-    portfolioTextContainer.appendChild(portfolioTitleContainer)
-    portfolioTextContainer.appendChild(portfolioDescriptionContainer)
-    portfolioTextContainer.appendChild(portfolioLinks)
-    portfolioItemHtml.appendChild(portfolioTextContainer)
-
-    container.appendChild(portfolioItemHtml)
+  container.appendChild(portfolioItemHtml);
 }
 
+dataPortfolioItems.forEach((element) => {
+    addPortfolioItems(portfolioListHtml, element);
+    console.log("portfolio item created");
+});
 
-dataPortfolioItems.forEach( (element) => {
-    console.log("portfolio item created")
-    addPortfolioItems(portfolioListHtml, element)
-})
+function isOverflown(element) {
+  return (
+    element.scrollHeight > element.clientHeight ||
+    element.scrollWidth > element.clientWidth
+  );
+}
 
+//ES6 version
+// const isOverflown = ({ clientWidth, clientHeight, scrollWidth, scrollHeight }) => {
+//     return scrollHeight > clientHeight || scrollWidth > clientWidth;
+// }
+
+const portfolios = document.getElementsByClassName("portfolioDescriptionContainer");
+
+for (let i = 0; i < portfolios.length; i++) {   
+    const el = portfolios[i];
+    if (isOverflown(el)) {
+    }
+}
 
 // let portfolioLinkValue = `
 //     <div>
 //         <a href="${arrayList.repoLink}" target="_blank">
-//             Source 
+//             Source
 //             <i class="fa-solid fa-display"></i>
 //         </a>
 //     </div>
 //     <div>
 //         <a href="${arrayList.liveLink}" target="_blank">
-//             Live 
+//             Live
 //             <i class="fab fa-github"></i>
 //         </a>
 //     </div>
 //     `;
 //     portfolioLinks.innerHtml = portfolioLinkValue;
-    // portfolioLinks.innerHtml = `
-    // <div>
-    //     <a href="${arrayList.repoLink}" target="_blank">
-    //         Source 
-    //         <i class="fa-solid fa-display"></i>
-    //     </a>
-    // </div>
+// portfolioLinks.innerHtml = `
+// <div>
+//     <a href="${arrayList.repoLink}" target="_blank">
+//         Source
+//         <i class="fa-solid fa-display"></i>
+//     </a>
+// </div>
 
-    // <div>
-    //     <a href="${arrayList.liveLink}" target="_blank">
-    //         Live 
-    //         <i class="fab fa-github"></i>
-    //     </a>
-    // </div>
-    // `;
+// <div>
+//     <a href="${arrayList.liveLink}" target="_blank">
+//         Live
+//         <i class="fab fa-github"></i>
+//     </a>
+// </div>
+// `;
---
+++ possibility-to-use/dataPortfolioItems.js
@@ -0,0 +1,110 @@
+function addPortfolioItems(container, arrayList) {
+    //  -----   create item's components
+    let portfolioItemHtml = document.createElement("div");
+  
+  //   let imageContainer = document.createElement("div");
+  //   let imageAnchor = document.createElement("a");
+  //   let imageHtml = document.createElement("img");
+  
+  //   let portfolioTitleContainer = document.createElement("div");
+  //   let portfolioDescriptionContainer = document.createElement("div");
+  //   let portfolioLinks = document.createElement("div");
+    // *container-ul care primeste tot continutul inafara de imagine
+    // let portfolioTextContainer = document.createElement('div')
+  
+    //  -----   add classes's components
+    //principal item
+    // portfolioItemHtml.classList.add('portfolio-item')
+    // //image
+    // imageContainer.classList.add('portfolio-image')
+    // imageAnchor.setAttribute('href', arrayList.liveLink)
+    // imageHtml.setAttribute('src', arrayList.imageLink)
+    // //text container
+    // portfolioTitleContainer.classList.add('portfolio-title')
+    // portfolioDescriptionContainer.classList.add('portfolio-description')
+    // portfolioLinks.classList.add('portfolio-links')
+    // portfolioTextContainer.classList.add('portfolio-text-container')
+  
+    portfolioItemHtml.innerHTML = `
+  
+      <div class="portfolio-item">
+          <div class="portfolio-image">
+              <a href=${arrayList.liveLink} target="_blank">
+                  <img src=${arrayList.imageLink} alt="">
+              </a>
+          </div>
+          <div class="portfolio-text-container">
+              <div class="portfolio-title">
+                  <h4>
+                      <a href="${arrayList.liveLink}">
+                          ${arrayList.title}
+                      </a>
+                  </h4>
+              </div>
+              <div class="portfolio-description">
+                  <p>
+                      ${arrayList.description} 
+                      <button class="readmore-btn">Read more</button>
+                      <button class="readmore-btn">Read more</button>
+                  </p>
+              </div>
+              <div class="portfolio-links">
+                  <div>
+                      <a href="${arrayList.repoLink}" target="_blank">
+                          Source 
+                          <i class="fa-solid fa-display"></i>
+                      </a>
+                  </div>
+  
+                  <div>
+                      <a href="${arrayList.liveLink}" target="_blank">
+                          Live 
+                          <i class="fab fa-github"></i>
+                      </a>
+                  </div>
+              </div>
+          </div>
+          </div>
+      </div>
+  `;
+  
+    //  -----   add content
+    // portfolioTitleContainer.innerHTML = `
+    // <h4>
+    //     <a href="${arrayList.liveLink}">
+    //         ${arrayList.title}
+    //     </a>
+    // </h4>`;
+    // portfolioDescriptionContainer.innerHTML = `
+    // <p>${arrayList.description}
+    //     <button class="readmore-btn">Read more</button>
+    //     <button class="readmore-btn">Read more</button>
+    // </p>`;
+    // portfolioLinks.innerHTML = `
+    // <div>
+    //     <a href="${arrayList.repoLink}" target="_blank">
+    //         Source
+    //         <i class="fa-solid fa-display"></i>
+    //     </a>
+    // </div>
+  
+    // <div>
+    //     <a href="${arrayList.liveLink}" target="_blank">
+    //         Live
+    //         <i class="fab fa-github"></i>
+    //     </a>
+    // </div>
+    // `;
+  
+    //   ----   append childs to parents containers
+    // imageContainer.appendChild(imageAnchor)
+    // imageAnchor.appendChild(imageHtml)
+  
+    // portfolioItemHtml.appendChild(imageContainer)
+    // portfolioTextContainer.appendChild(portfolioTitleContainer)
+    // portfolioTextContainer.appendChild(portfolioDescriptionContainer)
+    // portfolioTextContainer.appendChild(portfolioLinks)
+    // portfolioItemHtml.appendChild(portfolioTextContainer)
+  
+    container.appendChild(portfolioItemHtml);
+  }
