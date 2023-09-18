5c4ecacc0568cd84c7ad04115bf1affe4af35390
bogdan-muntean <https://github.com/bogdan-muntean>
2023-09-18 11:38:53 +0300

    add workexperience data table and edit the README

---
+++ src/data/dataWorkexperience.js
@@ -0,0 +1,11 @@
+// Create the database for Workexperience items.
+
+export let dataPortfolioItems = [
+    {
+        title: `RPA Developer Internship`,
+        span: "Automatify",
+        timeData: "07/2023 - 09/2023",
+        text: "",
+        icon: `<i class="fa-solid fa-display"></i>`,
+      }
+]
