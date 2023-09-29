160b1c3564de159a8dda05a55f2ac5b5e286db7c
bogdan-muntean <https://github.com/bogdan-muntean>
2023-09-29 13:14:08 +0300

    variables and style changes for about_me-page

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { pageTransitions } from "./utils/pageTransitions.js";
 import { toggleLightMode } from "./utils/toggleLightMode.js";
 
 // Add the Page Transitions function for section buttons.
-pageTransitions(".control");
+pageTransitions(".control-btn");
 
 // Add toggle light mode function for theme-btn.
 toggleLightMode(".theme-btn");
