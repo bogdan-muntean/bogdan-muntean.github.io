7a0724814cc3ef3e845e5140ec8d6e81104de841
Bogdan <bogdan.muntean@digitaldistribution.io>
2025-07-29 13:07:26 +0300

    +

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ import { pageTransitions } from "./utils/pageTransitions.js";
 import { toggleLightMode } from "./utils/toggleLightMode.js";
 
 // Add the Page Transitions function for section buttons.
-pageTransitions(".control-btn");
+// pageTransitions(".control-btn");
+document.addEventListener('DOMContentLoaded', function () {
+  const navIcon = document.getElementById('nav-icon');
+  const mobileMenu = document.getElementById('menu');
+  const menuLinks = document.querySelectorAll('.menu-link');
+
+  navIcon.addEventListener('click', function () {
+    navIcon.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+  });
+
+  menuLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navIcon.classList.remove('open');
+      mobileMenu.classList.remove('open');
+    });
+  });
+});
 
 // Add toggle light mode function for theme-btn.
 toggleLightMode(".theme-btn");
